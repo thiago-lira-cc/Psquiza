c289702e40237a572b175e5250cb13e07322d84a
119110826 IELE FACUNDO PASSOS <ielefp@lcc3-010.lcc.ufcg.edu.br>
2019-11-05 09:47:45 -0300

    teste do pesquisador atualizados

--- testes_unidade/Testes/TestControllerPesquisador.java
+++ testes_unidade/Testes/TestControllerPesquisador.java
@@ -15,6 +15,7 @@ class TestControllerPesquisador {
 	public void novoControllerPesquisador() {
 		controlePesquisador = new ControllerPesquisador();
 		controlePesquisador.cadastraPesquisador("Macaco Louco","externo", "Interessado nos efeitos do elemento X nos seres vivos","crazy.monkey@2000", "http://loko");
+		controlePesquisador.cadastraPesquisador("Biroliro", "externo", "Interessado na fiscalizacao da vida sexual alheia(via twitter)", "bozo@2018", "https://bolsolixo");
 	}
 	
 	//cadastro pesquisador
@@ -95,13 +96,119 @@ class TestControllerPesquisador {
 	@Test
 	public void testaExibePesquisador() {
 		assertEquals("Macaco Louco (externo) - Interessado nos efeitos do elemento X nos seres vivos - crazy.monkey@2000 - http://loko", controlePesquisador.exibePesquisador("crazy.monkey@2000"));
+		assertEquals("Biroliro (externo) - Interessado na fiscalizacao da vida sexual alheia(via twitter) - bozo@2018 - https://bolsolixo", controlePesquisador.exibePesquisador("bozo@2018"));
+	}
+	
+	@Test
+	public void exibeEmailInvalido() {
+		try {
+			controlePesquisador.exibePesquisador("lala@land.com");
+			assertFalse(false);
+		}
+		catch(IllegalArgumentException m) {
+			
+		}
+	}
+	
+	@Test
+	public void exibeEmailVazio() {
+		try {
+			controlePesquisador.exibePesquisador("");
+			assertFalse(false);
+		}
+		catch(IllegalArgumentException m) {
+			
+		}
+	}
+	
+	
+	// altera pesquisador
+	
+	@Test
+	public void alteraPesquisador() {
+		assertEquals("Biroliro (externo) - Interessado na fiscalizacao da vida sexual alheia(via twitter) - bozo@2018 - https://bolsolixo", controlePesquisador.exibePesquisador("bozo@2018"));
+		controlePesquisador.alteraPesquisador("bozo@2018", "FOTO", "https://elenao");
+		assertEquals("Biroliro (externo) - Interessado na fiscalizacao da vida sexual alheia(via twitter) - bozo@2018 - https://elenao", controlePesquisador.exibePesquisador("bozo@2018"));
+	}
+	
+	@Test
+	public void alteraEmailVazio() {
+		try {
+			controlePesquisador.alteraPesquisador("","FUNCAO","professor");
+			assertFalse(false);
+		}
+		catch(IllegalArgumentException m) {
+			
+		}
+	}
+	
+	@Test
+	public void alteraAtributoInvalido(){
+		try {
+			controlePesquisador.alteraPesquisador("bozo@2018", "BOCA", "calada");
+			assertFalse(false);
+		}
+		catch(IllegalArgumentException m) {
+			
+		}
+	}
+	@Test
+	public void alteraEmailInvalido() {
+		try {
+			controlePesquisador.alteraPesquisador("bozo.lixoso@elenao.com", "FUNCAO", "aluno");
+			assertFalse(false);
+		}
+		catch(IllegalArgumentException m){
+			
+		}
+	}
+	
+	@Test
+	public void alteraAtributoVazio() {
+		try {
+			controlePesquisador.alteraPesquisador("bozo@2018", "", "aluno");
+			assertFalse(false);
+		}
+		catch(IllegalArgumentException m) {
+			
+		}
+	}
+	@Test
+	public void alteraNovoValorVazio() {
+		try {
+			controlePesquisador.alteraPesquisador("bozo@2018", "FUNCAO", "");
+			assertFalse(false);
+		}
+		catch(IllegalArgumentException m) {
+			
+		}
 	}
 	
 	// status pesquisador 
 	@Test
 	public void testaPesquisadorEhAtivado() {
+		controlePesquisador.desativaPesquisador("bozo@2018");
 		assertEquals(true, controlePesquisador.pesquisadorEhAtivado("crazy.monkey@2000"));
-		// desativar pesquisa p testar
+		assertEquals(false,controlePesquisador.pesquisadorEhAtivado("bozo@2018"));
+	}
+	
+	@Test 
+	public void verificaEmail() {
+		try {
+			controlePesquisador.pesquisadorEhAtivado("biroloro@socorrodeus.com");
+			assertFalse(false);
+		}
+		catch (IllegalArgumentException m) {
+			
+		}
+		
+		try {
+			controlePesquisador.pesquisadorEhAtivado("");
+			assertFalse(false);
+		}
+		catch (IllegalArgumentException m){
+			
+		}
 	}
 }
 
